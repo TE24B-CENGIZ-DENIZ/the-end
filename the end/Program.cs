03ec04abe43be19aa20053a699be247afd23b4da
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2026-03-03 14:59:45 +0100

    zxdfgdffd

--- the end/Program.cs
+++ the end/Program.cs
@@ -1,1 +1,54 @@
-﻿// cyberpunk rouglike
+﻿// // cyberpunk rouglike intro
+// Console.WriteLine("The end is nigh");
+// Console.WriteLine("Hi player you are here to kill the bad guys ");
+// Console.WriteLine(" chose a character and kill them if you can");
+
+
+// string[] character= ["toy t-rex", "cyborg 1", "penutbutter"];
+
+// Console.WriteLine($" your characters are {character}");
+// string characterChosen= Console.ReadLine();
+
+
+// // dificulties
+// // -------------------------------------------------------methods-------------------------------------------------------------
+
+
+
+// Console.ReadLine();
+
+
+
+
+
+
+
+
+
+static void answerCorrecter(int setAmoutOfAnswers, string[] textOnTheBord)
+{
+
+
+    string playerAnswer = Console.ReadLine();
+
+
+    for (int i = 0; i < setAmoutOfAnswers - 1; i++)
+    {
+        while (playerAnswer != textOnTheBord[i])
+        {
+            Console.WriteLine("pick a character");
+        }
+
+        break;
+
+    }
+
+}
+
+answerCorrecter(2, ["test", "test 2"]);
+
+Console.ReadLine();
+
+
+
+
