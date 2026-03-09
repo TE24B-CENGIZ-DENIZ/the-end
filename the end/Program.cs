9e056737c6bb69bbe9f4fbb31b39e6d3b2d5580e
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2026-03-09 09:41:27 +0100

    sdfsdf

--- the end/Program.cs
+++ the end/Program.cs
@@ -1,13 +1,13 @@
-﻿// // cyberpunk rouglike intro
-// Console.WriteLine("The end is nigh");
-// Console.WriteLine("Hi player you are here to kill the bad guys ");
-// Console.WriteLine(" chose a character and kill them if you can");
+﻿ // cyberpunk rouglike intro
+ Console.WriteLine("The end is nigh");
+ Console.WriteLine("Hi player you are here to kill the bad guys ");
+ Console.WriteLine(" chose a character and kill them if you can");
 
 
-// string[] character= ["toy t-rex", "cyborg 1", "penutbutter"];
+ string[] character= ["toy t-rex", "cyborg 1", "penutbutter"];
 
-// Console.WriteLine($" your characters are {character}");
-// string characterChosen= Console.ReadLine();
+ Console.WriteLine($" your characters are {character}"); //doesn't work yet
+ answerCorrecter(3, ["toy t-rexy", "cyborg 1", "penutbutter" ]);
 
 
 // // dificulties
@@ -25,27 +25,45 @@
 
 
 
-static void answerCorrecter(int setAmoutOfAnswers, string[] textOnTheBord)
+static void answerCorrecter(int setAmoutOfAnswers, string[] textOnTheBord) //komplex algortihm
 {
-
-
-    string playerAnswer = Console.ReadLine();
-
+    string playerAnswer = "";
 
     for (int i = 0; i < setAmoutOfAnswers - 1; i++)
     {
-        while (playerAnswer != textOnTheBord[i])
+        while (playerAnswer != textOnTheBord[i])  
         {
-            Console.WriteLine("pick a character");
-        }
+            playerAnswer = Console.ReadLine();
 
-        break;
+            if (playerAnswer != textOnTheBord[i])
+            {
+                Console.WriteLine("pick a character");
+            }
+
+        }
 
     }
 
 }
 
-answerCorrecter(2, ["test", "test 2"]);
+
+
+//the other way for  answercorrecttor
+// string[] answerlist= ["hi", "hello"];
+// Console.WriteLine(answerlist[0] );
+// Console.WriteLine(answerlist[1]);
+
+// string playerAnswer= "";
+
+// while (playerAnswer != "hi" && playerAnswer != "hello" )
+// {
+//     playerAnswer= Console.ReadLine();
+
+//     if (playerAnswer != "hi" && playerAnswer != "hello")
+//     {
+//         Console.WriteLine("wrong pick another character");
+//     }
+// }
 
 Console.ReadLine();
 
