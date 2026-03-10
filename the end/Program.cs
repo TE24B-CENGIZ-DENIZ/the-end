b6f75072ac78014dcb493cbd545dc92f96a0c3d1
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2026-03-10 14:48:58 +0100

    fresh mind

--- the end/Program.cs
+++ the end/Program.cs
@@ -1,69 +1,78 @@
-﻿ // cyberpunk rouglike intro
- Console.WriteLine("The end is nigh");
- Console.WriteLine("Hi player you are here to kill the bad guys ");
- Console.WriteLine(" chose a character and kill them if you can");
+﻿//  // cyberpunk rouglike intro
+//  Console.WriteLine("The end is nigh");
+//  Console.WriteLine("Hi player you are here to kill the bad guys ");
+//  Console.WriteLine(" chose a character and kill them if you can");
 
 
- string[] character= ["toy t-rex", "cyborg 1", "penutbutter"];
+//  string[] character= ["toy t-rex", "cyborg 1", "penutbutter"];
 
- Console.WriteLine($" your characters are {character}"); //doesn't work yet
- answerCorrecter(3, ["toy t-rexy", "cyborg 1", "penutbutter" ]);
-
-
-// // dificulties
-// // -------------------------------------------------------methods-------------------------------------------------------------
-
+//  Console.WriteLine(" your characters are ");
+//  Console.WriteLine(character[0]);
+//  Console.WriteLine(character[1]);
+//  Console.WriteLine(character[2]);
+//  answerCorrecter(3, ["toy t-rexy", "cyborg 1", "penutbutter" ]);
 
 
-// Console.ReadLine();
+// // // dificulties
+// // // -------------------------------------------------------methods-------------------------------------------------------------
+
+
+// static void answerCorrecter(int setAmoutOfAnswers, string[] textOnTheBord) //komplex algortihm
+// {
+//     string playerAnswer = ""; // extract to see which charachter is picked
+
+//     for (int i = 0; i < setAmoutOfAnswers - 1; i++)
+//     {
+//         while (playerAnswer != textOnTheBord[i])  
+//         {
+//             playerAnswer = Console.ReadLine();
+
+//             if (playerAnswer != textOnTheBord[i])
+//             {
+//                 Console.WriteLine("pick a character");
+
+//             }
+
+//         }
+
+//     }
+
+
+
+// }
+
+
+
+// //the other way for  answercorrecttor
+// // string[] answerlist= ["hi", "hello"];
+// // Console.WriteLine(answerlist[0] );
+// // Console.WriteLine(answerlist[1]);
+
+// // string playerAnswer= "";
+
+// // while (playerAnswer != "hi" && playerAnswer != "hello" )
+// // {
+// //     playerAnswer= Console.ReadLine();
+
+// //     if (playerAnswer != "hi" && playerAnswer != "hello")
+// //     {
+// //         Console.WriteLine("wrong pick another character");
+// //     }
+// // }
 
 
 
 
-
-
-
-
-
-static void answerCorrecter(int setAmoutOfAnswers, string[] textOnTheBord) //komplex algortihm
+// charachter designer devide into dmg and name designer
+static void characterDesigner(string characterName ,  int regularAttackDmg, string regularAttackName ,int superAttackDmg , string superAttackName)
 {
-    string playerAnswer = "";
-
-    for (int i = 0; i < setAmoutOfAnswers - 1; i++)
-    {
-        while (playerAnswer != textOnTheBord[i])  
-        {
-            playerAnswer = Console.ReadLine();
-
-            if (playerAnswer != textOnTheBord[i])
-            {
-                Console.WriteLine("pick a character");
-            }
-
-        }
-
-    }
-
+  //charachter name is == player answer after the loop is done for that is done so that it becomes one of the correct answers
+ 
 }
 
 
+//need type converting (stamina option), random chance,pseudo code,if and else, return valiue, 
 
-//the other way for  answercorrecttor
-// string[] answerlist= ["hi", "hello"];
-// Console.WriteLine(answerlist[0] );
-// Console.WriteLine(answerlist[1]);
-
-// string playerAnswer= "";
-
-// while (playerAnswer != "hi" && playerAnswer != "hello" )
-// {
-//     playerAnswer= Console.ReadLine();
-
-//     if (playerAnswer != "hi" && playerAnswer != "hello")
-//     {
-//         Console.WriteLine("wrong pick another character");
-//     }
-// }
 
 Console.ReadLine();
 
