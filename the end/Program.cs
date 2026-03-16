00b794962d078ca002fc53a8a429c6d7159ac547
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2026-03-16 09:31:39 +0100

    worked on returning playeranswe but didn't go as planed but i might be able to fix and can't currently put public code on a seperet file

--- the end/Program.cs
+++ the end/Program.cs
@@ -10,36 +10,36 @@
 //  Console.WriteLine(character[0]);
 //  Console.WriteLine(character[1]);
 //  Console.WriteLine(character[2]);
-//  answerCorrecter(3, ["toy t-rexy", "cyborg 1", "penutbutter" ]);
+
+
 
 
 // // // dificulties
 // // // -------------------------------------------------------methods-------------------------------------------------------------
 
 
-// static void answerCorrecter(int setAmoutOfAnswers, string[] textOnTheBord) //komplex algortihm
-// {
-//     string playerAnswer = ""; // extract to see which charachter is picked
+static void answerCorrecter(int setAmoutOfAnswers, string[] textOnTheBord) //komplex algortihm
+{
+  string playerAnswer = ""; // extract to see which charachter is picked
 
-//     for (int i = 0; i < setAmoutOfAnswers - 1; i++)
-//     {
-//         while (playerAnswer != textOnTheBord[i])  
-//         {
-//             playerAnswer = Console.ReadLine();
+  for (int i = 0; i < setAmoutOfAnswers - 1; i++)
+  {
+    while (playerAnswer != textOnTheBord[i])
+    {
+      playerAnswer = Console.ReadLine();
 
-//             if (playerAnswer != textOnTheBord[i])
-//             {
-//                 Console.WriteLine("pick a character");
+      if (playerAnswer != textOnTheBord[i])
+      {
+        Console.WriteLine("pick a character");
 
-//             }
+      }
 
-//         }
+    }
 
-//     }
+  }
 
-
-
-// }
+ 
+}
 
 
 
@@ -64,12 +64,30 @@
 
 
 // charachter designer devide into dmg and name designer
-static void characterDesigner(string characterName ,  int regularAttackDmg, string regularAttackName ,int superAttackDmg , string superAttackName)
+static void characterDesigner( int regularAttackDmg, string regularAttackName, int superAttackDmg, string superAttackName)
 {
+
+  
+
   //charachter name is == player answer after the loop is done for that is done so that it becomes one of the correct answers
- 
+
+
+
 }
 
+
+// static (int,int,int) hi(string hi)
+// {
+//   int hel= 4;
+//   int of= 5;
+//   int yey= 6;
+
+
+//   return(hel,of,yey);
+
+
+//test
+// }
 
 //need type converting (stamina option), random chance,pseudo code,if and else, return valiue, 
 
