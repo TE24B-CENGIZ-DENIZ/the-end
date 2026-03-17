e354e48ae2678183aaf5de2a187924d9c5ee2fca
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2026-03-17 15:07:14 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- the end/Program.cs
+++ the end/Program.cs
@@ -1,44 +1,47 @@
-﻿//  // cyberpunk rouglike intro
-//  Console.WriteLine("The end is nigh");
-//  Console.WriteLine("Hi player you are here to kill the bad guys ");
-//  Console.WriteLine(" chose a character and kill them if you can");
+﻿
+
+// cyberpunk rouglike intro
+Console.WriteLine("The end is nigh");
+Console.WriteLine("Hi player you are here to kill the bad guys ");
+Console.WriteLine(" chose a character and kill them if you can");
 
 
-//  string[] character= ["toy t-rex", "cyborg 1", "penutbutter"];
+string[] character = ["toy t-rex", "cyborg 1", "penutbutter"];
 
-//  Console.WriteLine(" your characters are ");
-//  Console.WriteLine(character[0]);
-//  Console.WriteLine(character[1]);
-//  Console.WriteLine(character[2]);
+Console.WriteLine(" your characters are ");
+Console.WriteLine(character[0]);
+Console.WriteLine(character[1]);
+Console.WriteLine(character[2]);
+answerCorrecter(3, ["toy t-rex", "cyborg 1", "penutbutter"]);
 
 
-
-
-// // // dificulties
+// // // dificulties complex alg
 // // // -------------------------------------------------------methods-------------------------------------------------------------
 
 
 static void answerCorrecter(int setAmoutOfAnswers, string[] textOnTheBord) //komplex algortihm
 {
   string playerAnswer = ""; // extract to see which charachter is picked
+  playerAnswer = Console.ReadLine();
 
   for (int i = 0; i < setAmoutOfAnswers - 1; i++)
   {
+
     while (playerAnswer != textOnTheBord[i])
     {
-      playerAnswer = Console.ReadLine();
-
-      if (playerAnswer != textOnTheBord[i])
+     
+      
+      if (playerAnswer != textOnTheBord[setAmoutOfAnswers-1])
       {
         Console.WriteLine("pick a character");
-
       }
+      
 
     }
 
   }
 
- 
+
 }
 
 
@@ -63,33 +66,14 @@ static void answerCorrecter(int setAmoutOfAnswers, string[] textOnTheBord) //kom
 
 
 
-// charachter designer devide into dmg and name designer
-static void characterDesigner( int regularAttackDmg, string regularAttackName, int superAttackDmg, string superAttackName)
-{
 
-  
+//need type converting (stamina option), random chance,pseudo code,if and else, return valiue, do not need complex algorittms for the entire code
 
-  //charachter name is == player answer after the loop is done for that is done so that it becomes one of the correct answers
+
+//story
 
 
 
-}
-
-
-// static (int,int,int) hi(string hi)
-// {
-//   int hel= 4;
-//   int of= 5;
-//   int yey= 6;
-
-
-//   return(hel,of,yey);
-
-
-//test
-// }
-
-//need type converting (stamina option), random chance,pseudo code,if and else, return valiue, 
 
 
 Console.ReadLine();
