fb20f276606a86e111346cc6f5309cabba8e8731
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2026-04-14 14:56:27 +0200

    Done

--- the end/Program.cs
+++ the end/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 // last part make a loop for fighting in the end do a random powerup decided by a dice  
 // note: if you cannot do the thing you need with the variables you have try a new variable instead.
-// cyberpunk rouglike intro
+// cyberpunk turn based fighting system 
 Console.WriteLine("The end is nigh");
 Console.WriteLine("Hi player you are here to kill the bad guys ");
 Console.WriteLine(" chose a character and kill them if you can");
@@ -10,15 +10,20 @@ Console.WriteLine(" chose a character and kill them if you can");
 string[] character = ["toy t-rex", "cyborg 1", "penutbutter"];
 
 Console.WriteLine(" your characters are ");
-Console.WriteLine(character[0]);
-Console.WriteLine(character[1]);
-Console.WriteLine(character[2]);
+Console.WriteLine($"1: {character[0]}");
+Console.WriteLine($"2: {character[1]}");
+Console.WriteLine($"3: {character[2]}");
+Console.WriteLine("pick the number of the character that you want");
 
-AnswerCorrecter(3, ["toy t-rex", "cyborg 1", "penutbutter"]);
-Dificulties();
-Fighter(1, 2); // place holder 
+AnswerCorrecter(3, ["1", "2", "3"]);
+// int HeroHp = 0;
+// int HeroDamage = 0;
+// int EnemyHp = 0;
+// int Enemydamage = 0;
+// (HeroHp, HeroDamage, Enemydamage, EnemyHp) = Dificulties();
+// Fighter(HeroHp, HeroDamage, Enemydamage, EnemyHp);
 
-// // // dificulties complex alg
+
 // // // -------------------------------------------------------methods-------------------------------------------------------------
 
 
@@ -33,23 +38,25 @@ static string AnswerCorrecter(int SetAmoutOfAnswers, string[] TextOnTheBord) //k
     for (int i = 0; i < SetAmoutOfAnswers; i++)
     {
 
-      if (PlayerAnswer != TextOnTheBord[i])
-      {
-
-        Console.WriteLine("pick one of the options");
-      }
-      else
+      if (PlayerAnswer == TextOnTheBord[i])
       {
         AnswerdCorrect = true;
         Console.WriteLine("you did it");
 
+
       }
+
 
     }
 
     if (AnswerdCorrect == true)
     {
       break;
+    }
+    else
+    {
+      Console.WriteLine("pick one of the options"); 
+
     }
 
   }
@@ -90,7 +97,7 @@ static (int, int, int, int) Dificulties() // ++ hp and dmg for enemy
   Console.WriteLine($"*------{DificultyList[1]}-----*");
   Console.WriteLine($"*------{DificultyList[2]}-----*");
 
-  string NewPlayerAnswer = AnswerCorrecter(3, ["easy baby mode", "normale", "eliv"]); //rtrn var
+  string NewPlayerAnswer = AnswerCorrecter(3, ["easy baby mode", "normale", "eliv"]); //rtrn val
 
   if (NewPlayerAnswer == "easy baby mode")
   {
@@ -123,43 +130,49 @@ static (int, int, int, int) Dificulties() // ++ hp and dmg for enemy
 
 }
 
-static void Fighter(int HeroHp, int EnemyHp) //  nvm you get the insert valiue via transformation of the return valiues 
+static void Fighter(int HeroHp, int HeroDamage, int Enemydamage, int EnemyHp) //  nvm you get the insert valiue via transformation of the return valiues 
 {
   // end of every loop return the hp of enemys and hero 
   Console.WriteLine("you now get to fight ");
-  Console.WriteLine("how many times would you like to attack? pick from 1 to 3 ");
-  while (EnemyHp >= 0 || HeroHp >= 0) // can be && instead
+
+  while (EnemyHp > 0 || HeroHp > 0) // can be && instead
   {
+    Console.WriteLine("how many times would you like to attack? pick from 1 to 3 ");
+
     int AmountOfAttacks;
     string PlayerAnswer = AnswerCorrecter(3, ["1", "2", "3"]);
     Console.WriteLine($"you attack {PlayerAnswer} amount of times");
-    int.TryParse(PlayerAnswer, out AmountOfAttacks); //do not need a bool bc answr cortctr allready checks 
-     // amount of attacks * dmg = enemy hp - new dmg = new enemy hp 
-    Console.WriteLine("it does {} damage");
+    int.TryParse(PlayerAnswer, out AmountOfAttacks); //do not need a bool bc answr correcter allready checks 
+
+    EnemyHp = EnemyHp - AmountOfAttacks * HeroDamage; //= new enemy hp 
+
+    Console.WriteLine($"it does {AmountOfAttacks * HeroDamage} damage");
     Console.WriteLine($"enemy has now {EnemyHp} hp");
 
 
     Console.WriteLine("the enemy attacks");
-    //hero hp- enemy dmg  = new hero hp
-    Console.WriteLine("it does {} damage");
+    HeroHp = HeroHp - Enemydamage; //hero hp- enemy dmg  = new hero hp
+    Console.WriteLine($"it does {Enemydamage} damage");
     Console.WriteLine($"you have now {HeroHp} hp");
 
-
-
+    if (HeroHp <= 0)
+    {
+      Console.WriteLine(" you lose");
+      break;
+    }
+    else if (EnemyHp <= 0)
+    {
+      Console.WriteLine("you win");
+      break;
+    }
 
   }
 
-  if (HeroHp <= 0)
-  {
-    Console.WriteLine(" you lose");
-  }
-  else if (EnemyHp <= 0)
-  {
-    Console.WriteLine("you win");
-  }
+
 }
 
-//need type converting , pseudo code  
+//need pseudo code 
+// current agenda QOL
 
 
 
